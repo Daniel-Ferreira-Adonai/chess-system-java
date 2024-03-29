5449dd1e3c73cb81929876f7a014a08baafca4b5
Daniel Felix <d.calpi100@hotmail.com>
2024-03-29 11:10:16 -0300

    começando a implementar peças e o tabuleiro de xadrez

--- src/Program.java
+++
@@ -1,6 +0,0 @@
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Program {
-    public static void main(String[] args) {
-
-    }
-}
--- src/application/Program.java
+++ src/application/Program.java
@@ -5,8 +5,6 @@ import boardgame.*;
 public class Program {
 
     public static void main(String[] args) {
-        Position position = new Position(3,5);
-        System.out.println(position);
-        System.out.println(position.getRow());
+        Board board = new Board(8,8);
     }
 }
